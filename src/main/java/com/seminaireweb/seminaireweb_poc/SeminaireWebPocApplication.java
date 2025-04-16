15e80efae4f88635648fc139ab45c05c53947fb4
Cédric <86252008+popille@users.noreply.github.com>
2025-04-16 09:37:26 +0200

    SimpleAuth

--- src/main/java/com/seminaireweb/seminaireweb_poc/SeminaireWebPocApplication.java
+++ src/main/java/com/seminaireweb/seminaireweb_poc/SeminaireWebPocApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SeminaireWebPocApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SeminaireWebPocApplication.class, args);
 	}
