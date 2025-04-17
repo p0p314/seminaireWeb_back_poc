5d7102543e30c585b3f734ad7891db08d6e070a1
Cédric <86252008+popille@users.noreply.github.com>
2025-04-17 09:07:54 +0200

    code pin ok

--- src/main/java/com/seminaireweb/seminaireweb_poc/entity/Credit_cards.java
+++ src/main/java/com/seminaireweb/seminaireweb_poc/entity/Credit_cards.java
@@ -5,7 +5,7 @@ import lombok.Data;
 
 @Entity
 @Data
-@Table(name = "credit_cards")
+@Table(name = "cards")
 public class Credit_cards {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
